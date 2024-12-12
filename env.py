d352432ffe56084dcc9da403fc505955d5cf2a32
Yazdan Ahmed <yazahmed17@outlook.com>
2024-12-12 16:24:17 +0000

    changes to nav

---
+++ env.py
@@ -0,0 +1,11 @@
+import os
+
+
+os.environ["DATABASE_URL"] = "postgresql://neondb_owner:BmX03DynvPHV@ep-divine-term-a2nn0w74.eu-central-1.aws.neon.tech/untie_legal_spiny_338166"
+
+
+os.environ["SECRET_KEY"] = "hotelFuturama"
+
+os.environ["CLOUDINARY_URL"] = "cloudinary://531476925134795:RxCQFay03MsOM7MQOn907KFwL-8@djg3tze0l"
+
+os.environ["DEBUG"] = "1"
